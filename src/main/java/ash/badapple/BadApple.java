cb2b6abb82f17b7c5e7a86ccb49e57c4e72609ac
apaww <patrickleedevmail@gmail.com>
2025-02-23 11:44:36 +0300

    Initial commit

---
+++ src/main/java/ash/badapple/BadApple.java
@@ -0,0 +1,16 @@
+package ash.badapple;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BadApple implements ModInitializer {
+	public static final String MOD_ID = "badapple";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/ash/badapple/BadAppleClient.java
@@ -0,0 +1,10 @@
+package ash.badapple;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BadAppleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
